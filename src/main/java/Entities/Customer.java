5545e5a5c76450df2ff03acf91daa76402491d40
Deimantas Gedgaudas <gedgaudasdei@gmail.com>
2019-05-06 23:03:57 +0300

    Connecting to H2 DB, extending player entity

--- src/main/java/Entities/Customer.java
+++ src/main/java/Entities/Customer.java
@@ -1,17 +1,46 @@
 package Entities;
 
-import java.io.Serializable;
+import Usecases.Customers;
 
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "CUSTOMER")
 public class Customer implements Serializable {
 
+    @Size(max = 50)
+    @Column(name = "NAME")
     private String name;
+
+    @Size(max = 50)
+    @Column(name = "SURNAME")
     private String surname;
+
+    @Size(max = 15)
+    @Column(name = "PHONENUMBER")
     private String phoneNumber;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
 
     public Customer(String name, String surname, String phoneNumber) {
         this.name = name;
         this.surname = surname;
         this.phoneNumber = phoneNumber;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -36,5 +65,19 @@ public class Customer implements Serializable {
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(id, customer.id) ||
+                (Objects.equals(name, customer.name) && Objects.equals(surname, customer.surname));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, surname);
     }
 }
